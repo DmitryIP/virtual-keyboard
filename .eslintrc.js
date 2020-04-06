25be71eb513a091e52e6ba7f86bc6d527a81d834
DmitryIP <dmitryip@tut.by>
2020-04-06 16:41:24 +0300

    feat: add keyboard

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ["airbnb/base"],
+  rules: {
+    semi: ["warn", "always"],
+    quotes: ["warn", "double"],
+    indent: [2, 4, {
+      "SwitchCase": 1
+    }],
+    'eol-last': ["error", "never"]
+  }
+};
